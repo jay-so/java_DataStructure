df4dfebba87c3a098eb8411b53ac8998eb11473b
jay-so <merry9504@naver.com>
2022-12-14 23:54:49 +0900

    자료구조 3장

---
+++ src/part3/SeqSearch.java
@@ -0,0 +1,14 @@
+package part3;
+
+public class SeqSearch {
+    //요솟수가 n인 배열 a에서 key와 값이 같은 요소를 검색
+    static int seqSearch(int[]a,int n, int key){
+        int i = 0;
+
+        while(true){
+            if(i==n)
+                return -1; //검색 실패(-1)반환
+
+        }
+    }
+}
